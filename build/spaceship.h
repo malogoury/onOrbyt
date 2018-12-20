1cbe8731c474586e5808996c83fa21773fd82e20
Malo Goury du Roslan <malo.goury@gmail.com>
2018-12-20 16:06:02 +0100

    merge conflict

--- build/spaceship.h
+++ build/spaceship.h
@@ -1,28 +1,29 @@
 
-//{{BLOCK(mami)
+//{{BLOCK(spaceship)
 
 //======================================================================
 //
-//	mami, 32x32@8, 
+//	spaceship, 32x32@8, 
+//	Transparent color : 00,00,00
 //	+ palette 256 entries, not compressed
 //	+ 16 tiles not compressed
 //	Total size: 512 + 1024 = 1536
 //
-//	Time-stamp: 2018-12-19, 13:04:57
+//	Time-stamp: 2018-12-19, 13:04:58
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
 
-#ifndef GRIT_MAMI_H
-#define GRIT_MAMI_H
+#ifndef GRIT_SPACESHIP_H
+#define GRIT_SPACESHIP_H
 
-#define mamiTilesLen 1024
-extern const unsigned int mamiTiles[256];
+#define spaceshipTilesLen 1024
+extern const unsigned int spaceshipTiles[256];
 
-#define mamiPalLen 512
-extern const unsigned short mamiPal[256];
+#define spaceshipPalLen 512
+extern const unsigned short spaceshipPal[256];
 
-#endif // GRIT_MAMI_H
+#endif // GRIT_SPACESHIP_H
 
-//}}BLOCK(mami)
+//}}BLOCK(spaceship)
